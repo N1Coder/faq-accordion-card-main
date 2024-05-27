c3eb3f4c1846557ad80cc5433f8fb94f98082f07
N1Coder <tomofanteng123@gmail.com>
2024-05-27 11:52:25 +0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,27 @@
+const accordionItems = document.querySelectorAll("details")
+const accordionTitles = document.querySelectorAll("summary")
+
+const closeAllItem = () => {
+  for (const accordionItem of accordionItems) {
+    accordionItem.removeAttribute("open")
+  }
+}
+
+for (const accordionItem of accordionItems) {
+  const summary = accordionItem.children[0]
+  const toggleAccordion = () => {
+    if (accordionItem.hasAttribute("open")) {
+      accordionItem.toggleAttribute("open")
+    } else {
+      closeAllItem()
+      accordionItem.toggleAttribute("open")
+    }
+  }
+
+  summary.addEventListener("click", toggleAccordion)
+
+  accordionItem.addEventListener("click", toggleAccordion)
+}
+
+// toggleAccordion(accordionItems)
+// toggleAccordion(accordionTitles)
